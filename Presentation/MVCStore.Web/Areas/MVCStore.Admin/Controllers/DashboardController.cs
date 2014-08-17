72a768845105b19c2ba963e22aec5b1e41b5b182
gauravbe <gauravshah2001@yahoo.com>
2014-08-17 11:29:00 -0500

    Added Admin layout and Dashboard

---
+++ Presentation/MVCStore.Web/Areas/MVCStore.Admin/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCStore.Admin.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        //
+        // GET: /Dashboard/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
